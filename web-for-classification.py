eaf6e250fc2086761b51198c723864a078322454
bylymenko <119964448+bylymenko@users.noreply.github.com>
2022-12-18 13:58:23 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -6,7 +6,7 @@ import numpy as np
 #from tensorflow.keras.models import Sequential
 #from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 #from tensorflow.keras import utils
-from tensorflow.keras.preprocessing import image
+#from tensorflow.keras.preprocessing import image
 #from tensorflow.keras.preprocessing import image_dataset_from_directory
 #import matplotlib.pyplot as plt
 #from google.colab import files
