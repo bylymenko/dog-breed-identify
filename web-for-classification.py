f2dec5f06ae5d5f421d67fe537da8abd86b9520c
bylymenko <bylymenko.anna1619@vu.cdu.edu.ua>
2023-05-18 14:12:30 +0300

    Fixed imports

--- web-for-classification.py
+++ web-for-classification.py
@@ -1,12 +1,10 @@
 import io
 import streamlit as st
 from PIL import Image
-import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
-from tensorflow.keras import utils
+from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+import numpy as np
 
 def load_model():
     return open("dog_breeds.h5")
